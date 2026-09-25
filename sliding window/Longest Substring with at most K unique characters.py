ecc3037ae3dada476a7a40389e1ee489554fc2dc
false200 <214800619+false200@users.noreply.github.com>
2026-09-25 23:46:20 +0530

    code

--- sliding window/Longest Substring with at most K unique characters.py
+++ sliding window/Longest Substring with at most K unique characters.py
@@ -1,21 +1,19 @@
 class Solution:
     def longestKSubstr(self, s, k):
         freq = {}
-        l = 0
+        l , r = 0 , 0
+        m = len(s)
         maxv = -1
 
-        for r in range(len(s)):
-            freq[s[r]] = freq.get(s[r], 0) + 1
-
+        while (r < m):
+            freq[s[r]] = freq.get(s[r] , 0 )  + 1
             while len(freq) > k:
                 freq[s[l]] -= 1
-
                 if freq[s[l]] == 0:
                     del freq[s[l]]
-
                 l += 1
-
             if len(freq) == k:
                 maxv = max(maxv, r - l + 1)
+            r += 1
 
         return maxv
